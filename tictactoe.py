2ceda2dbf872111d91aec08891ab9e88f8452355
berkanidu-01 <49951088+berkanidu-01@users.noreply.github.com>
2023-10-24 16:52:31 +0200

    Update tictactoe.py
    j'ai ajouter les 2 première fonction

--- tictactoe.py
+++ tictactoe.py
@@ -18,14 +18,31 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    x_count = 0
+    o_count = 0
+    for row in board:
+        for cell in row:
+            if cell == X:
+                x_count += 1
+            elif cell == O:
+                o_count += 1
+    if x_count > o_count:
+        return O
+    else:
+        return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                possible_actions.add((i, j))
+    return possible_actions
+    
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
